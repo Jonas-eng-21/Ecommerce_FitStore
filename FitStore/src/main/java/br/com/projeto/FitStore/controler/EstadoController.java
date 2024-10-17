e623a477b9ee181cdca7a063bd6d3523ac40c018
Jonas-eng-21 <jonas.eng.software@gmail.com>
2024-10-17 14:50:20 -0300

    remoção de comentários

--- FitStore/src/main/java/br/com/projeto/FitStore/controler/EstadoController.java
+++ FitStore/src/main/java/br/com/projeto/FitStore/controler/EstadoController.java
@@ -26,25 +26,24 @@ public class EstadoController {
 	@Autowired
 	private EstadoRepositorio estadoRepositorio;
 
-    // Listar todos os estados - GET /api/estados
     @GetMapping("/estados")
     public List<Estado> listarEstados() {
-        return estadoRepositorio.findAll(); // Retorna a lista de estados em JSON
+        return estadoRepositorio.findAll();
     }
 
-    // Obter estado por ID - GET /api/estado/{id}
+  
     @GetMapping("/estado/{id}")
     public Optional<Estado> obterEstadoPorId(@PathVariable Long id) {
-        return estadoRepositorio.findById(id); // Retorna o estado encontrado pelo ID
+        return estadoRepositorio.findById(id);
     }
 
-    // Cadastrar um novo estado - POST /api/estado
+
     @PostMapping("/estado")
     public Estado cadastrarEstado(@RequestBody Estado estado) {
-        return estadoRepositorio.save(estado); // Salva e retorna o estado cadastrado
+        return estadoRepositorio.save(estado);
     }
 
-    // Editar um estado existente - PUT /api/estado/{id}
+  
     @PutMapping("/estado/{id}")
     public Estado editarEstado(@PathVariable Long id, @RequestBody Estado estadoAtualizado) {
         Optional<Estado> estadoExistente = estadoRepositorio.findById(id);
@@ -52,15 +51,15 @@ public class EstadoController {
             Estado estado = estadoExistente.get();
             estado.setNome(estadoAtualizado.getNome());
             estado.setSigla(estadoAtualizado.getSigla());
-            return estadoRepositorio.save(estado); // Atualiza e retorna o estado
+            return estadoRepositorio.save(estado); 
         }
-        return null; // Se o estado não for encontrado, retorna null ou uma resposta apropriada
+        return null;
     }
 
-    // Remover um estado - DELETE /api/estado/{id}
+ 
     @DeleteMapping("/estado/{id}")
     public void removerEstado(@PathVariable Long id) {
-        estadoRepositorio.deleteById(id); // Remove o estado pelo ID
+        estadoRepositorio.deleteById(id);
     }
 	
 }
