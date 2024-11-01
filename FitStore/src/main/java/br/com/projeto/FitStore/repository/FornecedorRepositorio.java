1f1e3e8925b35e63f3dd5bde3e5427d3595b56ce
Jonas-eng-21 <jonas.eng.software@gmail.com>
2024-11-01 20:39:32 -0300

    Cadastro de usuário do tipo Fornecedor - andamento

---
+++ FitStore/src/main/java/br/com/projeto/FitStore/repository/FornecedorRepositorio.java
@@ -0,0 +1,8 @@
+package br.com.projeto.FitStore.repository;
+
+import br.com.projeto.FitStore.models.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepositorio extends JpaRepository<Fornecedor, Long> {
+
+}
