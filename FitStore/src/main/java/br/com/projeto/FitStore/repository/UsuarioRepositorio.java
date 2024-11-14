ab79372069e040d6957041f39e3bd58572e1852b
Jonas-eng-21 <jonas.eng.software@gmail.com>
2024-11-14 10:04:58 -0300

    Remoção do UsuarioRepositorio, não era mais necessário na aplicação

--- FitStore/src/main/java/br/com/projeto/FitStore/repository/UsuarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.com.projeto.FitStore.repository;
-
-import br.com.projeto.FitStore.models.UsuarioAutenticavel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioRepositorio extends JpaRepository<UsuarioAutenticavel, Long> {
-    Optional<UsuarioAutenticavel> findByEmail(String email);
-}
