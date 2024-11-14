e6fcfda38f4ff1a72c7ef652e52f2b861cd1c0db
Jonas-eng-21 <jonas.eng.software@gmail.com>
2024-11-14 11:11:39 -0300

    Mudança no AuthController - Retirei os parâmetros da URL na autenticação do usuário por questão de segurança e agora isso vai pelo corpo da requisição como JSON

--- FitStore/src/main/java/br/com/projeto/FitStore/service/AuthController.java
+++ FitStore/src/main/java/br/com/projeto/FitStore/service/AuthController.java
@@ -2,10 +2,7 @@ package br.com.projeto.FitStore.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -14,21 +11,26 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    public static class LoginRequest {
+        public String email;
+        public String senha;
+    }
+
     @PostMapping("/login/cliente")
-    public ResponseEntity<String> loginCliente(@RequestParam String email, @RequestParam String senha) {
-        String token = authService.loginCliente(email, senha);
+    public ResponseEntity<String> loginCliente(@RequestBody LoginRequest loginRequest) {
+        String token = authService.loginCliente(loginRequest.email, loginRequest.senha);
         return ResponseEntity.ok(token);
     }
 
     @PostMapping("/login/funcionario")
-    public ResponseEntity<String> loginFuncionario(@RequestParam String email, @RequestParam String senha) {
-        String token = authService.loginFuncionario(email, senha);
+    public ResponseEntity<String> loginFuncionario(@RequestBody LoginRequest loginRequest) {
+        String token = authService.loginFuncionario(loginRequest.email, loginRequest.senha);
         return ResponseEntity.ok(token);
     }
 
     @PostMapping("/login/fornecedor")
-    public ResponseEntity<String> loginFornecedor(@RequestParam String email, @RequestParam String senha) {
-        String token = authService.loginFornecedor(email, senha);
+    public ResponseEntity<String> loginFornecedor(@RequestBody LoginRequest loginRequest) {
+        String token = authService.loginFornecedor(loginRequest.email, loginRequest.senha);
         return ResponseEntity.ok(token);
     }
 }
