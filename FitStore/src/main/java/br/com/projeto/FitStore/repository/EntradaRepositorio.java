0097ffc074c37f8e54b81f2128edd1fa5326100d
gabreleu <salomao.gabrielm@upe.br>
2024-12-04 19:13:17 -0300

    Modificação no Back: ItemEntrada está cadastrando mais parâmetros do Produto agora

--- FitStore/src/main/java/br/com/projeto/FitStore/repository/EntradaRepositorio.java
+++ FitStore/src/main/java/br/com/projeto/FitStore/repository/EntradaRepositorio.java
@@ -1,8 +1,13 @@
 package br.com.projeto.FitStore.repository;
 
 import br.com.projeto.FitStore.models.Entrada;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EntradaRepositorio extends JpaRepository<Entrada, Long> {
+import java.util.List;
 
+public interface EntradaRepositorio extends JpaRepository<Entrada, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"itensEntrada.produto"})
+    List<Entrada> findAll();
 }
